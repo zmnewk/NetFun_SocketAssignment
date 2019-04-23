71b904160750dbe6c3bfcfad8a802668f9e62931
Z <zander.newcombe@gmail.com>
2019-04-23 20:52:48 +1000

    updates

--- SocketAssigment.py
+++ SocketAssigment.py
@@ -2,39 +2,57 @@
 from socket import *
 import sys # In order to terminate the program
 
+#Create a socket with family and type as default
 serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
-#Fill in start
-#Sets the serverPort integer
+
+#Sets the serverPort integer variable to a suitable port
 serverPort = 6000
-serverSocket.bind((socket.gethostname(), serverPort)) 
+
+#Bind Socket to an address. Format determined by family(AF_INET above)
+serverSocket.bind(('49.195.49.211', serverPort)) 
+
+#Tell socket to listen to up to 1 connection at a time
 serverSocket.listen(1)
 
-#Fill in end
 while True:
     #Establish the connection
     print('Ready to serve...')
+    
+    #Set up a new connection from the client
     connectionSocket, addr = serverSocket.accept()          
     try:
-        message = connectionSocket.recv(1024)  #Fill in start          #Fill in end               
+        #Take data recieved from client and store in variable message
+        message = connectionSocket.recv(1024)  #Fill in start                        
+        #filename is the 2nd item in HTTP header
         filename = message.split()[1]                 
+        
+        # Exclude "/" from filename and open file with that name
         f = open(filename[1:])                        
-        outputdata = #Fill in start       #Fill in end                   
+        
+        #Put data from the read file into variable outputdata
+        outputdata = f.read                 
         #Send one HTTP header line into socket
-        #Fill in start
-        #Fill in end                
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+                       
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):           
             connectionSocket.send(outputdata[i].encode())
         connectionSocket.send("\r\n".encode())
         
+        #Close client socket
         connectionSocket.close()
+        
     except IOError:
         #Send response message for file not found
-        #Fill in start        
-        #Fill in end
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode)
+        connectionSocket.send("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n".encode())
+
+        
         #Close client socket
-        #Fill in start
-        #Fill in end            
+        connectionSocket.close()
+        
+        
+        
 serverSocket.close()
 sys.exit()#Terminate the program after sending the corresponding data                                    
